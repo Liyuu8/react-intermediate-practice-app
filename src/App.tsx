659700aff3f46ede2f19d2030e93615396486818
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2021-11-21 19:55:10 +0900

    Add header layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import theme from "./theme/theme";
 import { Router } from "./router/Router";
+import { HeaderLayout } from "./templates/HeaderLayout";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
-        <Router />
+        <HeaderLayout>
+          <Router />
+        </HeaderLayout>
       </BrowserRouter>
     </ChakraProvider>
   );
